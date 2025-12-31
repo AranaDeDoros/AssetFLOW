dcce5743be70dc9a51c93e3a4938fc35881ded37
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-12-31 15:16:26 -0600

    helper functions

---
+++ src/main/scala/web/common/Common.scala
@@ -0,0 +1,41 @@
+package web.common
+
+import java.nio.file.Paths
+import java.time.{Instant, LocalDate}
+import java.time.format.DateTimeFormatter
+
+object Common {
+
+  def getNameAndExtension(fullPath: String): (String, Option[String]) = {
+    (getFileNameWithoutExtension(fullPath),getFileExtension(fullPath))
+  }
+
+  private def getFileExtension(fullPath: String): Option[String] = {
+    val fileName = Paths.get(fullPath).getFileName.toString
+    val lastDotIndex = fileName.lastIndexOf('.')
+    if (lastDotIndex != -1 && lastDotIndex < fileName.length - 1) {
+      Some(fileName.substring(lastDotIndex + 1))
+    } else {
+      None
+    }
+  }
+
+  private def getFileNameWithoutExtension(fileName: String): String = {
+    val lastDotIndex = fileName.lastIndexOf('.')
+    if (lastDotIndex > 0) fileName.substring(0, lastDotIndex)
+    else {
+      fileName
+    }
+  }
+
+  def timestamp : String =  {
+    Instant.now().toString
+  }
+
+  def today: String = {
+    val date = LocalDate.now
+    val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
+    date.format(formatter)
+  }
+
+}
